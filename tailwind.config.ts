1261f9cfc5774432f5ff7102ffcc9dda5add9e38
junyoung <jadw9715@gmail.com>
2025-02-07 01:15:50 +0900

    ⚙ init. styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,43 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        main: "url('/assets/images/bg/main-bg.jpg')",
+        login: "url('/assets/images/bg/login-bg.jpg')",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        transparent: "transparent",
+        white: "#ffffff",
+        kakao: "#FAE100",
+        main: {
+          100: "#D2D1D9",
+          200: "#A5A2B2",
+          300: "#77748C",
+          400: "#4A4565",
+          500: "#1D173F",
+          600: "#171232",
+          700: "#110E26",
+          800: "#0C0919",
+          900: "#06050D",
+        },
+        point: {
+          100: "#F5CEEB",
+          200: "#EB9DD7",
+          300: "#E26CC4",
+          400: "#D83BB0",
+          500: "#CE0A9C",
+          600: "#A5087D",
+          700: "#7C065E",
+          800: "#52043E",
+          900: "#29021F",
+        },
+      },
+      fontFamily: {
+        pretendard: ["Pretendard", "san-serif"],
+        dnf: ["DNFBitBitv2", "san-serif"],
+        pixelNes: ["Pixel_NES", "san-serif"],
       },
     },
   },
